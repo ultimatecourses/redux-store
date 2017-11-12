670a0f854cc14845f5a78e4046f1e03c835b2245
Todd Motto <todd@toddmotto.com>
2017-11-12 19:10:44 +0000

    Step 3: Setup reducers

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,11 @@ const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
 
-const store = new fromStore.Store(
-  {},
-  {
-    todos: [{ label: 'Eat pizza', complete: false }],
-  }
-);
+const reducers = {
+  todos: fromStore.reducer,
+};
+
+const store = new fromStore.Store(reducers);
 
 button.addEventListener(
   'click',
@@ -25,6 +24,8 @@ button.addEventListener(
       type: 'ADD_TODO',
       payload,
     });
+
+    console.log(store.value);
 
     input.value = '';
   },
--- src/store/store.ts
+++ src/store/store.ts
@@ -4,7 +4,8 @@ export class Store {
   private state: { [key: string]: any };
 
   constructor(reducers = {}, initialState = {}) {
-    this.state = initialState;
+    this.reducers = reducers;
+    this.state = this.reduce(initialState, {});
   }
 
   get value() {
@@ -12,10 +13,14 @@ export class Store {
   }
 
   dispatch(action) {
-    this.state = {
-      ...this.state,
-      todos: [...this.state.todos, action.payload],
-    };
-    console.log(this.state);
+    this.state = this.reduce(this.state, action);
+  }
+
+  private reduce(state, action) {
+    const newState = {};
+    for (const prop in this.reducers) {
+      newState[prop] = this.reducers[prop](state[prop], action);
+    }
+    return newState;
   }
 }
