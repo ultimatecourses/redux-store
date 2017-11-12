28f815671bd933725fe5859f6a5a98d1dc5eba75
Todd Motto <todd@toddmotto.com>
2017-11-12 18:27:25 +0000

    Step 1: Store setup and initialState

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
+import * as fromStore from './store';
+
 import { renderTodos } from './utils';
 
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
+
+const store = new fromStore.Store(
+  {},
+  {
+    todos: [{ label: 'Eat pizza', complete: false }],
+  }
+);
+
+console.log(store.value);
 
 button.addEventListener(
   'click',
