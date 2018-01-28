7f58382fca783d457d890e24b71d995301ee5205
ScrumMasterFletch <acfletch@uark.edu>
2018-01-28 14:37:11 -0600

    Added and utilized basic store

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import { renderTodos } from './utils';
+import * as fromStore from './store';
 
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
+
+const store = new fromStore.Store(
+  // reducers
+  {},
+  // initial state
+  {
+  todos: [{label:'Eat pizza', complete: false}],
+});
+
+console.log(store.value);
 
 button.addEventListener(
   'click',
@@ -12,6 +23,7 @@ button.addEventListener(
 
     const payload = { label: input.value, complete: false };
 
+    // dispatch action here
     console.log(payload);
 
     input.value = '';
